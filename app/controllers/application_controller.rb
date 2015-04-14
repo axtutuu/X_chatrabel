f767be22d7dd96bec4288d6c6ff7aedfd3fe5a20
kawasakiatsushi <kawasaki8910@gmail.com>
2015-04-15 02:19:49 +0900

    devise微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :add_params, if: :devise_controller?
+  before_filter :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
